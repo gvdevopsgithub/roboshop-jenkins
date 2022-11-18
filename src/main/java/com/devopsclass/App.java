2f3f9cd21ae11c29ec1b7930d8c56b77d2dcfb58
gv <gvdevops00@gmail.com>
2022-11-18 19:09:52 +0530

    updating roboshop-jenkins

---
+++ src/main/java/com/devopsclass/App.java
@@ -0,0 +1,13 @@
+package com.devopsclass;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World agian, from the cli!, A new change added for git jenkins automation testing, A new changeFInal" );
+    }
+}
